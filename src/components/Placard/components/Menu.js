06f198e9a02faa9d26860fdc68e473e3eec31c99
Alex Shortt <alexander.shortt@gmail.com>
2019-11-04 20:58:33 +0100

    add actions to menu

--- src/components/Placard/components/Menu.js
+++ src/components/Placard/components/Menu.js
@@ -1,10 +1,26 @@
 import React from "react"
 import styled from "styled-components/macro"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Container = styled.div`
   padding: 0.75rem 1rem;
   background: black;
   color: white;
+  display: flex;
+  align-items: flex-end;
+`
+
+const Info = styled.div`
+  flex: 1;
+`
+
+const Actions = styled.div`
+  height: 100%;
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const Title = styled.h1`
@@ -27,15 +43,32 @@ const Subtitle = styled.p`
   }
 `
 
+const RefreshIcon = styled(FontAwesomeIcon).attrs({ icon: "sync-alt" })`
+  color: white;
+  font-size: 0.8rem;
+  opacity: 0.8;
+  cursor: pointer;
+  transition: 0.15s opacity linear;
+
+  &:hover {
+    opacity: 1;
+  }
+`
+
 export default function Menu(props) {
   const { piece, ...restProps } = props
 
-  const { title = "Untitled", description = "", date } = piece
+  const { title = "Untitled", date } = piece
 
   return (
     <Container {...restProps}>
-      <Title>{title}</Title>
-      <Subtitle>{date}</Subtitle>
+      <Info>
+        <Title>{title}</Title>
+        <Subtitle>{date}</Subtitle>
+      </Info>
+      <Actions>
+        <RefreshIcon />
+      </Actions>
     </Container>
   )
 }
